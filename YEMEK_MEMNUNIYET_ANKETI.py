db4b0e5e48ea3f8fb601ad0e2fd42507ca17e1eb
pameksmalkaraidari <emrah@pameks.com.tr>
2026-08-08 13:03:28 +0300

    Update YEMEK_MEMNUNIYET_ANKETI.py

--- YEMEK_MEMNUNIYET_ANKETI.py
+++ YEMEK_MEMNUNIYET_ANKETI.py
@@ -357,7 +357,7 @@ with tab_degerlendirme:
             if k != "Tarih" and str(gunun_menusu[k]).strip()
         ]
 
-        st.markdown("Her ürünü ayrı ayrı değerlendir. **Kötü** seçersen kısa bir açıklama isteyeceğiz.")
+        st.markdown("Her ürünü ayrı ayrı değerlendir. **Kötü** olduğunu düşünüyorsan nedenini bize açıklar mısın?.")
         st.divider()
 
         degerlendirmeler = {}
